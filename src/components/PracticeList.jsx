c86549a889e52ca87802add60d99739ffd5fe691
Aapo <aapo.kiljunen@gmail.com>
2024-04-18 01:04:34 +0300

    some progress with db-querys

--- src/components/PracticeList.jsx
+++ src/components/PracticeList.jsx
@@ -1,6 +1,6 @@
-import { Box, Button, Grid, List, ListItem, ListItemText, Card, Typography, CardHeader, Avatar, IconButton, CardMedia, Tooltip } from "@mui/material";
+import { Box, Button, Grid, List, ListItem, ListItemText, Card, Typography, CardHeader, Avatar, IconButton, CardMedia, Tooltip, CardContent, CardActions, ToggleButton, FormControlLabel, Switch } from "@mui/material";
 import { useEffect, useState } from "react";
-import { fetchPractises } from "../service/Api";
+import { handleUpdatePractice, fetchPractises, getPractice } from "../service/Api";
 import { blueGrey, lightBlue, orange, red, yellow } from "@mui/material/colors";
 
 function PracticeList() {
@@ -16,10 +16,11 @@ function PracticeList() {
         3: blueGrey,
         4: yellow,
         5: orange,
+        'done': 'secondary.main',
     };
 
     const getTypeColor = (type) => {
-        return typeColors[type] || "gray";
+        return typeColors[type] || 'secondary';
     }
 
     const getPractices = async () => {
@@ -30,11 +31,6 @@ function PracticeList() {
             console.error('Virhe harjoitustietojen hakemisessa:', error);
         }
     }
-
-    useEffect(() => {
-        getPractices();
-    }, []);
-
     const filterType = (filterValue) => {
         setTypeFilter(filterValue);
         setInfo(`Näytetään harjoitukset tyypille ${filterValue}`);
@@ -54,12 +50,31 @@ function PracticeList() {
     }
 
     const FilterLink = ({ onClick, label, value }) => (
-        <a href='#' onClick={() => onClick(value)}>{label}</a>
+        <Typography component='span' sx={{ textDecoration: 'none', color: 'inherit', cursor: 'pointer' }} onClick={() => onClick(value)}>
+            {label}
+        </Typography>
     );
+
+    const handleChangeDone = async (event, id) => {
+        const checked = event.target.checked;
+        try {
+            const updateData = {columnName: 'done', newValue: checked ? 1 : 0, id: id};
+            await handleUpdatePractice(updateData);
+            const response = await getPractice(id);
+            const updatedPractice = response.data;
+            setPractices(prevList => prevList.map(item => item.id === id ? updatedPractice : item));
+        } catch (error) {
+            console.error('Virhe päivittäessä harjoitusta: ', error);
+        }
+    }
+
+    useEffect(() => {
+        getPractices();
+    }, []);
 
     if (practices.length === 0) {
         return (
-            <Typography variant="body1" align="center">
+            <Typography variant='body1' align='center'>
                 Ei näytettäviä harjoituksia
             </Typography>
         );
@@ -67,52 +82,65 @@ function PracticeList() {
 
     return (
         <Box sx={{ padding: 2 }}>
-            <Grid>
-                <List>
-                    {practices.map((practice, index) => {
-                        const showType = typeFilter.length === 0 || practice.type === typeFilter;
-                        const showLocation = locationFilter.length === 0 || practice.location === locationFilter;
-                        const date = new Date(practice.date);
-                        const formattedDate = date.toLocaleDateString('fi-FI')
+            <Grid container spacing={2}>
+                {practices.map((practice, index) => {
+                    const showType = typeFilter.length === 0 || practice.type === typeFilter;
+                    const showLocation = locationFilter.length === 0 || practice.location === locationFilter;
+                    const date = new Date(practice.date);
+                    const formattedDate = date.toLocaleDateString('fi-FI')
+                    const done = practice.done == 0 ? false : true;
 
-                        if (showType && showLocation) {
-                            return (
-                                <ListItem key={index}>
-                                    <Card sx={{ maxWidth: 300, bgcolor: getTypeColor(practice.typeId)[50] }}>
-                                        <CardHeader
-                                            avatar={
-                                                <Tooltip title={practice.typeName}>
-                                                    <Avatar sx={{ bgcolor: getTypeColor(practice.typeId)[500] }} aria-label="recipe">
-                                                        {practice.typeName.charAt(0)}
-                                                    </Avatar>
-                                                </Tooltip>
-                                            }
-                                            title={practice.description}
-                                            subheader={formattedDate}
-                                        />
+                    if (showType && showLocation) {
+                        return (
+                            <Grid item key={practice.id}>
+                                <Card sx={{ maxWidth: 300, bgcolor: getTypeColor(practice.done ? 'done' : practice.typeId)[100] }}>
+                                    <CardHeader
+                                        avatar={
+                                            <Tooltip title={practice.typeName}>
+                                                <Avatar sx={{ bgcolor: getTypeColor(practice.typeId)[500] }} aria-label='typeAvatar'>
+                                                    {practice.typeName.charAt(0)}
+                                                </Avatar>
+                                            </Tooltip>
+                                        }
+                                        title={practice.description}
+                                        subheader={formattedDate}
+                                    />
 
-                                        <CardMedia
-                                            component="img"
-                                            height="200"
-                                            image={practice.image || `src/assets/pics/defaultpics/${practice.typeId}.jpg`}
-                                            alt={`${practice.typeName} kuva`}
-                                            title={practice.typeName}
+                                    <CardMedia
+                                        component='img'
+                                        height='300'
+                                        image={practice.image || `src/assets/pics/defaultpics/${practice.typeId}.jpg`}
+                                        alt={`${practice.typeName} kuva`}
+                                        title={practice.typeName}
+                                    />
+
+                                    <CardContent>
+                                        <Typography variant='body2' color='text.secondary'>
+                                            <FilterLink onClick={filterType} label={practice.typeName} value={practice.typeName} /><br />
+                                            <FilterLink onClick={filterLocation} label={practice.location} value={practice.location} />
+                                        </Typography>
+                                    </CardContent>
+
+                                    <CardActions disableSpacing>
+                                        <FormControlLabel
+                                            control={<Switch checked={done} onChange={() => handleChangeDone(event, practice.id)} />}
+                                            label='Suoritettu'
                                         />
-                                        <FilterLink onClick={filterType} label={practice.typeName} value={practice.typeName} /><br />
-                                        <FilterLink onClick={filterLocation} label={practice.location} value={practice.location} /><br />
-                                        {practice.description}
-                                    </Card>
-                                </ListItem>
-                            );
-                        } else {
-                            return null;
-                        }
-                    })}
-                    {(typeFilter.length > 0 || locationFilter.length > 0) &&
-                        <div><Button variant="contained" onClick={clearFilters}>Tyhjennä suodattimet</Button></div>
+                                    </CardActions>
+
+                                </Card>
+                            </Grid>
+                        );
+                    } else {
+                        return null;
                     }
-                </List>
+                })}
             </Grid>
+            {(typeFilter.length > 0 || locationFilter.length > 0) &&
+                <div><Button sx={{ marginTop: 2 }} variant='contained' onClick={clearFilters}>Tyhjennä suodattimet</Button></div>
+            }
+
+            
         </Box >
     );
 }
--- src/service/Api.js
+++ src/service/Api.js
@@ -2,6 +2,30 @@ import axios from 'axios';
 
 const API_URL = 'http://localhost:8081';
 
-export const fetchPractises = () => {
-    return axios.get(`${API_URL}/practices`);
+export const fetchPractises = async () => {
+    try {
+        return await axios.get(`${API_URL}/practices`);
+    } catch (error)  {
+        console.error('Error fetching practices:', error);
+        throw error;
+    }
+};
+
+export const getPractice = async (id) => {
+    try {
+        return await axios.get(`${API_URL}/practice/${id}`);
+    } catch (error)  {
+        console.error('Error getting a practice:', error);
+        throw error;
+    }
+};
+
+export const handleUpdatePractice = async (updateData) => {
+    try {
+        const response = await axios.post(`${API_URL}/updatePractice`, updateData);
+        console.log(response.data); 
+    } catch (error) {
+        console.error('Error updating column:', error);
+        throw error;
+    }
 };
